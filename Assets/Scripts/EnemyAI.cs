b17f418116f0fe0fe6f4662a91c618354026a0ee
hukfu <astafyevaviola@gmail.com>
2024-02-24 18:25:39 +0300

    v_0.2[mob_ai]

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> PatrolPoints;
+
+    private NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        PickNewPatrolPoint();
+    }
+
+    void Update()
+    {
+        PatrolUpdate();
+    }
+
+    private void PatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoint();
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = PatrolPoints[Random.Range(0, PatrolPoints.Count)].position;
+    }
+}
